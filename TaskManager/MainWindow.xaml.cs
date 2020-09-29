43feec86b75b1e86fc0fd72d3c6f86e7fdbf866f
Toma <tzastrovskaya@gmail.com>
2020-09-29 18:13:17 +0300

    fix1

---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Windows;
+using TaskManager.Models;
+
+
+namespace TaskManager
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private BindingList<Model> _toDate;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _toDate = new BindingList<Model>()
+            {
+                new Model(){Task = "test"}
+            };
+
+        }
+    }
+}
---
+++ TaskManager/Models/Model.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+    class Model
+    {
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        private bool _Status;
+
+        public bool Status
+        {
+            get { return _Status; }
+            set { _Status = value; }
+        }
+
+        private string _Task;
+
+        public string Task
+        {
+            get { return _Task; }
+            set { _Task = value; }
+        }
+    }
+}
